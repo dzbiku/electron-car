8c4406ef2980a364459dc791840ef700fc0d1ac3
OnlyOne <dzbik.konrad@gmail.com>
2018-02-18 13:39:08 +0100

    office&txt  type

--- src/js/media.js
+++ src/js/media.js
@@ -51,7 +51,7 @@ function getFiles(dir, fileList) {
         fileList.push(files[i]);
       else {
         if (pathFile.extname(files[i]) === ".txt" || pathFile.extname(files[i]) === ".log" || pathFile.extname(files[i]) === ".ipynb") {
-          contentToShowInPage += '<div style="margin-left:25px;"> <a href="#" onclick="window.open(\'' + name + '\'); return false;" style="float: left;">' + files[i] + '</a></div></br>';
+          contentToShowInPage += '<div style="margin-left:25px;"><img id="logo" src="../assets/images/txt.png" alt="mp4_logo" style="width:20px;" /><a href="#" onclick="window.open(\'' + name + '\'); return false;"">' + files[i] + '</a></div>';
         }
         else if (pathFile.extname(files[i]) === ".mp4") {
           contentToShowInPage += '<div style="margin-left:25px;"><img id="logo" src="../assets/images/mp4.png" alt="mp4_logo" style="width:20px;" /> <a href="#" class="video_clip" id="video_single_' + files[i] + '\'">' + name + '</a></div>';
@@ -59,7 +59,9 @@ function getFiles(dir, fileList) {
         else if (pathFile.extname(files[i]) === ".mp3") {
           contentToShowInPage += '<div style="margin-left:25px;"><img id="logo" src="../assets/images/mp3_new.png" alt="mp3_logo" style="width:20px;" /> <a href="#" class="video_clip" id="video_single_' + files[i] + '\'">' + name + '</a></div>';
         }
-        // pathLocal.join(__dirname,'../assets/images/smile.png')
+        else if (pathFile.extname(files[i]) === ".doc" || pathFile.extname(files[i]) === ".docx" || pathFile.extname(files[i]) === ".xls"|| pathFile.extname(files[i]) === ".xlsx") {
+          contentToShowInPage += '<div style="margin-left:25px;"><img id="logo" src="../assets/images/office.png" alt="mp4_logo" style="width:20px;" /> <a href="#" onclick="window.open(\'' + name + '\'); return false;"">' + files[i] + '</a></div>';
+        }        
         //another content- in progress, what to do with files
         else {
           contentToShowInPage += '<div style="margin-left:25px;"><img id="logo" src="../assets/images/not_know.png" alt="mp3_logo" style="width:20px;" /> ' + files[i] + '</div>';
