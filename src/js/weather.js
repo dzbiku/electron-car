756846ec88e043a706f8a4597a00f23d20611e24
OnlyOne <dzbik.konrad@gmail.com>
2018-02-19 23:27:45 +0100

    jQuery $(func{...}); == java script body onload

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,7 +1,7 @@
-function gettingJSON(){
-    document.write("jquery loaded");
+$(function gettingJSON(){
+    //document.write("jquery loaded");
     $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London&APPID=9817b68be1114bce73d71d5678d36925",function(json){
         // document.write(JSON.stringify(json));
         console.log(JSON.stringify(json));
     });
-}
+});
