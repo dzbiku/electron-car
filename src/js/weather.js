1b6cab46034edbda56256fa5c4fdd90704d4309b
OnlyOne <dzbik.konrad@gmail.com>
2018-02-20 22:12:55 +0100

    ops, it's temperatur in kelvin

--- src/js/weather.js
+++ src/js/weather.js
@@ -18,19 +18,26 @@ function showPosition(position) {
 };
 
 function weatherAfterParse(jsonAfterParse) {
-    var objectToSetToHtml = '</br><div style="font-size: 25px">' + jsonAfterParse.name + ', ' + jsonAfterParse.sys.country + 
-    '</div><div>Wind:' + jsonAfterParse.weather[0].description + ', Speed: ' + jsonAfterParse.wind.speed + ' Direction: ' + jsonAfterParse.wind.deg + 
-    '</div><div>Humidity: ' + jsonAfterParse.main.humidity + 
-    '</div><div>Clouds: ' + jsonAfterParse.clouds.all + 
-    '</div><div style="font-size: 20px">Temp.: ' + jsonAfterParse.main.temp + ' F</div>';
+    var objectToSetToHtml = '</br><div style="font-size: 25px">' + jsonAfterParse.name + ', ' + jsonAfterParse.sys.country +
+        '</div><div>Sky:' + jsonAfterParse.weather[0].description.toUpperCase() + '</div><div>Wind speed: ' + jsonAfterParse.wind.speed + 'm/s, direction: ' + jsonAfterParse.wind.deg +
+        '°</div><div>Humidity: ' + jsonAfterParse.main.humidity +
+        '%</div><div>Clouds: ' + jsonAfterParse.clouds.all +
+        '%</div><div style="font-size: 20px">Temp.: ' + kelvinToCelsius(jsonAfterParse.main.temp).toFixed(2) + ' °C</div>';
     return objectToSetToHtml;
 };
+
+function fahrenheitToCelsius(tempInFahr) {
+    return (5 / 9) * (tempInFahr - 32);
+}
+function kelvinToCelsius(tempInFahr) {
+    return tempInFahr - 273.15;
+}
 
 waether_load.addEventListener('click', getLocation(), false)
 //download full json
 $(function gettingJSON() {
     $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Wroclaw&APPID=9817b68be1114bce73d71d5678d36925", function (json) { //by name
-        // $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" +long+ "&APPID=9817b68be1114bce73d71d5678d36925", function (json) {
+        // $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" +long+ "&APPID=9817b68be1114bce73d71d5678d36925", function (json) { //by cords
         //console.log('przed: ' + lat + '; po parsowaniu: ' + parseInt(lat));
         //console.log('przed: ' + long + '; po parsowaniu: ' + parseInt(long));
         //console.log(JSON.stringify(json));
