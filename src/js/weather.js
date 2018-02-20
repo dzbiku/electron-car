284ab18e2861b3ee248dcf63f148937b9e4e5dc1
OnlyOne <dzbik.konrad@gmail.com>
2018-02-20 21:56:03 +0100

    refactor

--- src/js/weather.js
+++ src/js/weather.js
@@ -3,29 +3,6 @@ var long;
 let positionsInGeo;
 //get geolocation using JS
 
-// class LocationGeo {
-//     constructor(x, y) {
-//         this.x = x;
-//         this.y = y;
-//         this.getLocation = function () {
-//             if (navigator.geolocation) {
-//                 navigator.geolocation.getCurrentPosition(this.setPosition);
-//             }
-//             else {
-//                 alert('Geolocation inn\'t supported by Your current device');
-//                 console.log('Geolocation inn\'t supported by Your current device');
-//             }
-//         };        
-//     }    
-//     setPosition (position) {
-//         this.x = position.coords.latitude;
-//         this.y = position.coords.longitude;
-//     }
-// }
-// const _location = new LocationGeo();
-// _location.getLocation();
-// console.log(_location.x + ' y: ' + _location.y);
-
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.watchPosition(showPosition);
@@ -52,10 +29,8 @@ function weatherAfterParse(jsonAfterParse) {
 waether_load.addEventListener('click', getLocation(), false)
 //download full json
 $(function gettingJSON() {
-    //document.write("jquery loaded");
     $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Wroclaw&APPID=9817b68be1114bce73d71d5678d36925", function (json) { //by name
         // $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" +long+ "&APPID=9817b68be1114bce73d71d5678d36925", function (json) {
-        // document.write(JSON.stringify(json));
         //console.log('przed: ' + lat + '; po parsowaniu: ' + parseInt(lat));
         //console.log('przed: ' + long + '; po parsowaniu: ' + parseInt(long));
         //console.log(JSON.stringify(json));
