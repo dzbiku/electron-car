96df172172f3c9c80aff0cd05791ecf7e7c67fa9
OnlyOne <dzbik.konrad@gmail.com>
2018-02-20 21:29:32 +0100

    weather api first steps using json in javascript and parse it to get weather description

--- src/js/weather.js
+++ src/js/weather.js
@@ -28,7 +28,7 @@ let positionsInGeo;
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.watchPosition(showPosition);
     } else {
         console.log('Geolocation inn\'t supported by Your current device');
     }
@@ -38,18 +38,30 @@ function showPosition(position) {
     long = position.coords.longitude;
     console.log('przed: ' + position.coords.latitude);
     console.log('przed: ' + position.coords.longitude);
-}
+};
 
-waether_load.addEventListener('click',getLocation(),false)
-// //download full json
-// $(function gettingJSON() {
-//     //document.write("jquery loaded");
-//     // $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London&APPID=9817b68be1114bce73d71d5678d36925", function (json) { //by name
-//     $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" +long+ "&APPID=9817b68be1114bce73d71d5678d36925", function (json) {
-//         // document.write(JSON.stringify(json));
-//         console.log('przed: ' + lat + '; po parsowaniu: ' + parseInt(lat));
-//         console.log('przed: ' + long + '; po parsowaniu: ' + parseInt(long));
-//         console.log(JSON.stringify(json));
-//     });
-// });
+function weatherAfterParse(jsonAfterParse) {
+    var objectToSetToHtml = '</br><div style="font-size: 25px">' + jsonAfterParse.name + ', ' + jsonAfterParse.sys.country + 
+    '</div><div>Wind:' + jsonAfterParse.weather[0].description + ', Speed: ' + jsonAfterParse.wind.speed + ' Direction: ' + jsonAfterParse.wind.deg + 
+    '</div><div>Humidity: ' + jsonAfterParse.main.humidity + 
+    '</div><div>Clouds: ' + jsonAfterParse.clouds.all + 
+    '</div><div style="font-size: 20px">Temp.: ' + jsonAfterParse.main.temp + ' F</div>';
+    return objectToSetToHtml;
+};
+
+waether_load.addEventListener('click', getLocation(), false)
+//download full json
+$(function gettingJSON() {
+    //document.write("jquery loaded");
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Wroclaw&APPID=9817b68be1114bce73d71d5678d36925", function (json) { //by name
+        // $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" +long+ "&APPID=9817b68be1114bce73d71d5678d36925", function (json) {
+        // document.write(JSON.stringify(json));
+        //console.log('przed: ' + lat + '; po parsowaniu: ' + parseInt(lat));
+        //console.log('przed: ' + long + '; po parsowaniu: ' + parseInt(long));
+        //console.log(JSON.stringify(json));
+        var jnObject = JSON.parse(JSON.stringify(json));
+        console.log(jnObject);
+        document.getElementById('weather-notify').innerHTML = weatherAfterParse(jnObject);
+    });
+});
 
