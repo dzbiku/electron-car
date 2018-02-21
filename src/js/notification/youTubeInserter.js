a591c5f87fc6b4a572c795d11218048a17c75303
OnlyOne <dzbik.konrad@gmail.com>
2018-02-21 21:56:49 +0100

    don't play video even if player ready

--- src/js/notification/youTubeInserter.js
+++ src/js/notification/youTubeInserter.js
@@ -22,7 +22,7 @@ function onYouTubeIframeAPIReady() {
 
 // 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
-  event.target.playVideo();
+  //event.target.playVideo();
 }
 
 // 5. The API calls this function when the player's state changes.
@@ -31,8 +31,9 @@ function onPlayerReady(event) {
 var done = false;
 function onPlayerStateChange(event) {
   if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
+    //setTimeout(stopVideo, 6000);
+    // event.stopVideo
+    // done = true;
   }
 }
 function stopVideo() {
