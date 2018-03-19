dddcc6fe84886f8cf4e0224dd32b9997968c2e09
OnlyOne <dzbik.konrad@gmail.com>
2018-03-19 22:55:31 +0100

    tmp changes

--- src/js/car.js
+++ src/js/car.js
@@ -1,1 +1,37 @@
 // google chart, type eng.
+google.charts.load('current', { 'packages': ['gauge'] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+    var data = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Gas', 80],
+        ['Engine x100', 55],
+        ['Fuel %', 68]
+    ]);
+
+    var options = {
+        width: 400, height: 120,
+        redFrom: 90, redTo: 100,
+        yellowFrom: 75, yellowTo: 90,
+        minorTicks: 5
+    };
+
+    var chart = new google.visualization.Gauge(document.getElementById('chart_div_gas'));
+
+    chart.draw(data, options);
+
+    setInterval(function () {
+        data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
+        chart.draw(data, options);
+    }, 13000);
+    setInterval(function () {
+        data.setValue(1, 1, 40 + Math.round(60 * Math.random()));
+        chart.draw(data, options);
+    }, 5000);
+    setInterval(function () {
+        data.setValue(2, 1, 60 + Math.round(20 * Math.random()));
+        chart.draw(data, options);
+    }, 26000);
+}
